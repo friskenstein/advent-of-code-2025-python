08581b4322e9fbada5637f4811c678bb433e1e31
Johan Frisk <johan@friskenstein.com>
2025-12-03 11:06:40 +0200

    day 3 part 2

--- day03/main.py
+++ day03/main.py
@@ -13,13 +13,27 @@ def max_two_digit_subsequence(s: str) -> int:
 
     return best_value
 
+def max_k_digit_subsequence(s: str, k: int) -> int:
+    n = len(s)
+	# assuming string valid and long enough
+
+    drop = n - k
+    stack: list[str] = []
+
+    for ch in s:
+        while drop and stack and stack[-1] < ch:
+            stack.pop()
+            drop -= 1
+        stack.append(ch)
+
+    return int(''.join(stack[:k]))
 
 def part1(data: str):
 	return sum(max_two_digit_subsequence(line) for line in data.split())
 
 
 def part2(data: str):
-	...
+	return sum(max_k_digit_subsequence(line, 12) for line in data.split())
 
 
 tc = '987654321111111\n811111111111119\n234234234234278\n818181911112111'
@@ -27,6 +41,8 @@ tc = '987654321111111\n811111111111119\n234234234234278\n818181911112111'
 def test_part1():
 	assert part1(tc) == 357
 
+def test_part2():
+	assert part2(tc) == 3121910778619
 
 if __name__ == "__main__":
 	with open("input.txt") as f:
